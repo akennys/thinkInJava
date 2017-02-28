9b02fcbd13253dea970001485df87abcbb07f02f
pingkang <pingkang@rongyi.com>
2017-02-28 11:10:39 +0800

    commit

--- src/typeinfo/InterfaceViolation.java
+++ src/typeinfo/InterfaceViolation.java
@@ -1,23 +1,29 @@
+package typeinfo;
+
 //: typeinfo/InterfaceViolation.java
 // Sneaking around an interface.
 import typeinfo.interfacea.*;
 
 class B implements A {
-  public void f() {}
-  public void g() {}
+	public void f() {
+	}
+
+	public void g() {
+	}
 }
 
 public class InterfaceViolation {
-  public static void main(String[] args) {
-    A a = new B();
-    a.f();
-    // a.g(); // Compile error
-    System.out.println(a.getClass().getName());
-    if(a instanceof B) {
-      B b = (B)a;
-      b.g();
-    }
-  }
-} /* Output:
-B
-*///:~
+	public static void main(String[] args) {
+		A a = new B();
+		a.f();
+		// a.g(); // Compile error
+		System.out.println(a.getClass().getName());
+		if (a instanceof B) {
+			System.out.println("123");
+			B b = (B) a;
+			b.g();
+		}
+	}
+} /*
+	 * Output: B
+	 */// :~
